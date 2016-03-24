946866b4134cae29ca858aee2ba53d2d728a6c01
Yusuke（祐介） Nakamura(中村） <yusukenakamura@yusukes-macbook-pro.local>
2016-03-24 15:37:34 -0500

    Initial Commit

---
+++ edu.Nakamura.TravelApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  edu.Nakamura.TravelApp
+//
+//  Created by Yusuke Nakamura on 3/24/16.
+//  Copyright © 2016 Yusuke Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
